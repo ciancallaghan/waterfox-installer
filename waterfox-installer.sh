dc687506a16e4175344df143aa567834c4d9f1cb
cian <cian@atlantis>
2021-12-28 09:23:10 +0100

    First push

---
+++ waterfox-installer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pkginfo=`curl -s "https://api.github.com/repos/WaterfoxCo/Waterfox/releases/latest" | grep linux`
+pkgname=`echo $pkginfo | grep name | head -n 1 | cut -d '"' -f 4`
+pkgurl=`echo $pkginfo | grep download_url | head -n 1 | cut -d '"' -f 8`
+
+echo $pkginfo
+echo $pkgname
+echo $pkgurl
+
+wget $pkgurl
+tar -xf $pkgname -C /opt
+cp ./waterfox.desktop /usr/share/applications/waterfox.desktop
+cp ./waterfox.svg /usr/share/icons/waterfox.svg
+ln -s /opt/waterfox/waterfox /usr/bin/waterfox
