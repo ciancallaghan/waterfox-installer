2df07b7b06391e0f47031c66c72feaa902f489fa
Cian <cian@rhuidean>
2022-05-11 10:46:26 +0200

    Added check for root privelages

--- waterfox-installer.sh
+++ waterfox-installer.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+if [[ $EUID != 0 ]]; then
+		echo "Please run as root/sudo"
+		exit
+fi
+
 pkginfo=`curl -s "https://api.github.com/repos/WaterfoxCo/Waterfox/releases/latest" | grep linux`
 pkgname=`echo $pkginfo | grep name | head -n 1 | cut -d '"' -f 4`
 pkgurl=`echo $pkginfo | grep download_url | head -n 1 | cut -d '"' -f 8`
-
-echo $pkginfo
-echo $pkgname
-echo $pkgurl
 
 wget $pkgurl
 tar -xf $pkgname -C /opt
--- waterfox-uninstaller.sh
+++ waterfox-uninstaller.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID != 0 ]]; then
+		echo "Please run as root/sudo"
+		exit
+fi
+
 rm -r /opt/waterfox
 rm /usr/share/applications/waterfox.desktop
 rm /usr/share/icons/waterfox.svg
